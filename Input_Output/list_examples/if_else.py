48ff8b9785ed2af09530824370604f85ae96c440
Padmaraj M <padma108raj@gmail.com>
2020-12-21 20:01:37 +0530

    Adding List and Tuples examples

---
+++ Input_Output/list_examples/if_else.py
@@ -0,0 +1,5 @@
+n=eval(input("Enter the Number:"))
+if n%2 == 0:
+    print(n, " is a even number")
+else:
+    print(n, " is odd number")
---
+++ Input_Output/list_examples/ordering_list.py
@@ -0,0 +1,7 @@
+n=[10,20,30,40,50]
+n.reverse()
+print(n)
+l=[30,64,23,11,34,52,33,22]
+print("Unsorted list",l)
+l.sort()
+print("Sorted list:",l)
