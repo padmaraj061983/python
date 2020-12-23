b480536c8fa05233f3fe305ba3eb0756314a10c6
Padmaraj M <padma108raj@gmail.com>
2020-12-23 21:02:53 +0530

    Modules and OOPS

---
+++ Modules_OOPS/modules_random_math.py
@@ -0,0 +1,7 @@
+import math  
+from random import *
+help(math)
+list=['a','b','c','d']
+for i in range(10):
+    print(choice(list))
+    
---
+++ Modules_OOPS/reloading_module.py
@@ -0,0 +1,8 @@
+import time
+from imp import reload
+import calculator
+time.sleep(30)
+reload(calculator)
+time.sleep(30)
+reload(calculator)
+print("The is reload testing")
